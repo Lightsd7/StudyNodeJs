33d7605330cd158eb8d816a5f42119d42bad6356
Raphael Molina <raphael.bedran@konduto.com>
2020-09-01 18:24:45 -0300

    First Commit
    Desenvolvendo em NodeJs

---
+++ src/app/rotas/rotas.js
@@ -0,0 +1,44 @@
+const LivroDao = require("../infra/livro-dao");
+const db = require("../../config/database");
+
+module.exports = (app) => {
+    app.get("/", function (req, resp) {
+        resp.send(
+            `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <title>Casa do Código</title>
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body>
+            </html>
+        `
+        );
+    });
+
+    app.get("/livros", function (req, resp) {
+        const livroDao = new LivroDao(db);
+        livroDao
+            .lista()
+            .then((livros) =>
+                resp.marko(require("../views/livros/lista/lista.marko"), {
+                    livros: livros,
+                })
+            )
+            .catch((erro) => console.log(erro));
+
+        // livroDao.lista(function (erro, resultados) {
+        //     resp.marko(require("../views/livros/lista/lista.marko"), {
+        //         livros: resultados,
+        //     });
+        // });
+
+        // db.all("SELECT * FROM livros", function (erro, resultados) {
+        //     resp.marko(require("../views/livros/lista/lista.marko"), {
+        //         livros: resultados,
+        //     });
+        // });
+    });
+};
